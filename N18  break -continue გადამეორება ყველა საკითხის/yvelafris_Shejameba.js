f536233a754e019aaaf60795463dfdcf8b15d74e
Maya kvaratskhelia <74621259+mayakvaratskhelia@users.noreply.github.com>
2022-10-05 23:30:12 +0400

    კიდევ ცოტა   ( - _ -)

--- N18  break -continue გადამეორება ყველა საკითხის/yvelafris_Shejameba.js
+++ N18  break -continue გადამეორება ყველა საკითხის/yvelafris_Shejameba.js
@@ -27,3 +27,47 @@ var monacemebi = {
 // გამოაქ ასე 
 
 // მეორე მაგალთი ობიექტზე
+
+const car = {
+  carName:` მერსედესი`,
+  carage: 1997,
+  carcolor: `წითელი`,
+  fullName_car: function () {
+    return this.carName + ' ' + this.carcolor + ' ' + this.carage;
+  }
+
+}
+//console.log(car.fullName_car());
+
+//მესამე  მასივი 
+
+const all_color = [`წითელი`,`ყვითელი`,`მწვანე `,`შავი`,`თეთრი`,`იასამნისფერი`,`ლურჯი`,`ყავისფერი`,`სტაფილოსფერი`];
+//console.log(all_color);
+
+for (let i = 0; i < all_color.length; i++) {
+ //console.log(all_color[i]);
+}
+// წითელი
+// ყვითელი
+// მწვანე 
+// შავი
+// თეთრი
+// იასამნისფერი
+// ლურჯი
+// ყავისფერი
+// სტაფილოსფერი
+
+
+const xili =[`ვაშლი`, `მსხალი `,`ატამი`, `ბალი`, `ბანანი`, `ვაშლატამა`, `ქლიავი`, `ჟოლო`];
+
+for(let i = 0; i < xili.length; i++){
+ //console.log(xili[i]);
+}
+
+//მეოთხე ფუნქცია
+
+function akademia(x) {
+  console.log(`ააწყვე აკადემიის საიტი  ${x} ცალი `);
+}
+akademia(25)
+
